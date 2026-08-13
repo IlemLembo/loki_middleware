44c28dcff4072bdf6b8cf6e2a0ba9c3dad956c44
Ilem Isaiah Nelson Junior LEMBO <110841264+IlemLembo@users.noreply.github.com>
2026-08-13 12:33:13 +0000

    Move to 0.1.9

---
+++ tests/conftest.py
@@ -0,0 +1,22 @@
+# tests/test_django_middleware.py
+import django
+from django.conf import settings
+
+def pytest_configure():
+    """Minimal Django configuration required for middleware testing."""
+    if not settings.configured:
+        settings.configure(
+            SECRET_KEY="test-secret-key-for-loki-middleware",
+            DEBUG=True,
+            ALLOWED_HOSTS=["*"],
+            ROOT_URLCONF=__name__,
+            MIDDLEWARE=[
+                "loki_middleware.django.DjangoLokiMiddleware",
+            ],
+            DATABASES={},
+            INSTALLED_APPS=[
+                "django.contrib.contenttypes",
+                "django.contrib.auth",
+            ],
+        )
+        django.setup()
